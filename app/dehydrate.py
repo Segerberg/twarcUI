26d1f02f235fb81f0c27a53e6fc48aa0533afc20
segerberg <andreassegerberg@gmail.com>
2018-02-03 09:40:46 +0100

    Fixed stupied misstake

--- app/dehydrate.py
+++ app/dehydrate.py
@@ -12,7 +12,7 @@ def dehydrateUserSearch(id):
     if not os.path.isdir(EXPORTS_BASEDIR):
         os.makedirs(EXPORTS_BASEDIR)
     q = models.TWITTER.query.filter(models.TWITTER.row_id == id).first()
-    with open(os.path.join(EXPORTS_BASEDIR, '{}_simple_dehydrate_UUID_{}.txt'.format(q.title, export_uuid)), 'w+') as f:
+    with open(os.path.join(EXPORTS_BASEDIR, '{}_dehydrate_UUID_{}.txt'.format(q.title, export_uuid)), 'w+') as f:
         for filename in os.listdir(os.path.join(ARCHIVE_BASEDIR,q.title)):
             if filename.endswith(".gz"):
                 for line in gzip.open(os.path.join(ARCHIVE_BASEDIR,q.title,filename)):
@@ -20,7 +20,7 @@ def dehydrateUserSearch(id):
                     tweet = json.loads(line.decode('utf-8'))['id_str']
                     f.write(tweet)
                     f.write('\n')
-    addExportRef = models.EXPORTS(url='{}_simple_dehydrate_UUID_{}.txt'.format(q.title, export_uuid),type='Dehydrate',exported=datetime.now(),count=count)
+    addExportRef = models.EXPORTS(url='{}_dehydrate_UUID_{}.txt'.format(q.title, export_uuid),type='Dehydrate',exported=datetime.now(),count=count)
     q.exports.append(addExportRef)
     db.session.commit()
     db.session.close()
@@ -37,20 +37,23 @@ def dehydrateCollection(id):
         tags
     dbDateStart = q.inclDateStart
     dbDateStop = q.inclDateEnd
-    with open(os.path.join(EXPORTS_BASEDIR,'{}_simple_dehydrate_UUID_{}.txt'.format(q.title,export_uuid)),'w+') as f:
+    with open(os.path.join(EXPORTS_BASEDIR,'{}_dehydrate_UUID_{}.txt'.format(q.title,export_uuid)),'w+') as f:
         for target in linkedTargets:
             print (target.title)
             for filename in os.listdir(os.path.join(ARCHIVE_BASEDIR,target.title)):
                 if filename.endswith(".gz"):
                     for line in gzip.open(os.path.join(ARCHIVE_BASEDIR,target.title,filename)):
+
                         count = count + 1
-                        tweet = json.loads(line.decode('utf-8'))['id_str']
+                        tweet = json.loads(line.decode('utf-8'))
                         tweetDate = datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+
                         if tweetDate > dbDateStart and tweetDate < dbDateStop:
-                            f.write(tweet)
+                            f.write(tweet['id_str'])
                             f.write('\n')
 
-    addExportRef = models.EXPORTS(url='{}_simple_dehydrate_UUID_{}.txt'.format(q.title, export_uuid),
+
+    addExportRef = models.EXPORTS(url='{}_dehydrate_UUID_{}.txt'.format(q.title, export_uuid),
                                   type='Dehydrate', exported=datetime.now(), count=count)
     q.exports.append(addExportRef)
     db.session.commit()
